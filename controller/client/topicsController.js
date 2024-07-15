b9dcfebc15261cd4899eeda88a129b67c34a947b
truong1003 <121231275+truong1003@users.noreply.github.com>
2024-07-15 19:44:08 +0700

    Client Topics

--- controller/client/topicsController.js
+++ controller/client/topicsController.js
@@ -6,7 +6,8 @@ class index{
         const topics = await Topics.find({deleted:false})
 
         res.render("client/pages/topics/index.pug",{
-            titlePage:"Chủ đề bài hát"
+            titlePage:"Chủ đề bài hát",
+            topics: topics
         })
     }
 }
